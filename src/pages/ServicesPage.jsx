bbf4ca957d7d668c1e4b400c39112f82db0ddbac
aymane-farajallah <aymanhajiqa@gmail.com>
2024-04-29 10:19:55 +0100

    FAQ

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ServicesHero from '../components/servicespage/serviceshero';
 import Plans from '../components/servicespage/plans';
+import Faq from '../components/servicespage/faq';
 
 const ServicesPage = () => {
   return (
     <>
         <ServicesHero/>
         <Plans/>
+        <Faq/>
         </>
   )
 }
