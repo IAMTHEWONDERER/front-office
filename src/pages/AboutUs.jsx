f43d59e4cee202922ebbe8e25672bdd466c83266
aymane-farajallah <aymanhajiqa@gmail.com>
2024-04-29 12:57:55 +0100

    ABOUT US HERO DONE

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Aboutushero from '../components/AboutUspage/heroaboutus';
+
+const AboutUs = () => {
+  return (
+    <>
+        <Aboutushero/>
+
+    </>
+  )
+}
+
+export default AboutUs;
