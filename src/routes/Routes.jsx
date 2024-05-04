147afdff7588831ee91ba39d3a903816f8f3561c
aymane-farajallah <aymanhajiqa@gmail.com>
2024-05-04 22:28:23 +0100

    none

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,11 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import ServicesPage from "../pages/ServicesPage";
 import AboutUs from "../pages/AboutUs";
-// import About from "../pages/about";
-// import Tour from "../pages/tour";
-// import Camping from "../pages/camping";
-// import Login from "../pages/login";
-// import Register from "../pages/register";
 
 const Router = () => {
   return (
