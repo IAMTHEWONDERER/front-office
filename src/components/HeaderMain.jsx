b9001095e535ab972c852627fcd70b5f8457cad8
Oussama Alouche <theoblivionitself@gmail.com>
2024-05-04 23:00:00 +0100

    pushing this

--- src/components/HeaderMain.jsx
+++ src/components/HeaderMain.jsx
@@ -7,17 +7,7 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="backdrop-blur-sm  bg-black bg-opacity-50 text-white p-4 flex items-center justify-between font-koulen">
-      <div className="flex items-center ">
-        <a href="home"><img src={logo} alt="Logo" className="h-8 mr-4" /></a>
-      </div>
-      <div className="hidden md:flex items-center space-x-4">
-        <a href="home" className="text-white">Home</a>
-        <a href="about-us" className="text-white">About Us</a>
-        <a href="services" className="text-white">Services</a>
-        <button className="  px-4 py-2 bg-white hover:bg-gray-300 text-black rounded transition duration-300 ease-in-out">Become a coach</button>
-        <button className="bg-red-600 hover:bg-red-900 text-white px-4 py-2 rounded transition duration-300 ease-in-out">Sign in</button>
-
+   
     <nav className="backdrop-blur-sm  bg-black bg-opacity-70 text-white pt-4 pr-4 pb-4 flex items-center justify-between font-koulen">
       <div className="flex items-center ">
         <img src={logo} alt="Logo" className="h-8 mr-4 pl-4" />
