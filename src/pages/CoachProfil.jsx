5b31f4672d7eeac9b9f5d5deb02c017901282275
Oussama Alouche <theoblivionitself@gmail.com>
2024-05-15 12:24:41 +0100

    adding a coachProfil page where it shows you the coach, more about them and other profils similar

--- src/pages/CoachProfil.jsx
+++ src/pages/CoachProfil.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const UserDashboard = () => {
+const CoachProfil = () => {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-12 md:py-24 lg:py-32">
       <div className="container mx-auto px-4 md:px-6">
@@ -216,4 +216,4 @@ function StarIcon(props) {
   )
 }
 
-export default UserDashboard;
+export default CoachProfil;
